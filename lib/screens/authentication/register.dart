8f1bfdd36f7b2adc902752f64701b1730e5ccbe5
Shashank Prasad <skshashankkumar41@gmail.com>
2020-08-06 05:10:43 +0530

    added loading widget

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/services/auth.dart';
 import 'package:todo_app/shared/decoration.dart';
+import 'package:todo_app/shared/loading.dart';
 
 class Register extends StatefulWidget {
   final Function toggleView;
@@ -15,84 +16,93 @@ class _RegisterState extends State<Register> {
   String email = '';
   String pass = '';
   String error = '';
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.lightGreen[200],
-        appBar: AppBar(
-          backgroundColor: Colors.lightGreen[500],
-          title: Text("Sign Up to ToDo App"),
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-                onPressed: () {
-                  widget.toggleView();
-                },
-                icon: Icon(Icons.person),
-                label: Text("Sign In"))
-          ],
-        ),
-        body: Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            child: Form(
-              key: _formkey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                    validator: (val) {
-                      if (val.isEmpty) {
-                        return 'Enter an Email';
-                      } else {
-                        return null;
-                      }
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.lightGreen[200],
+            appBar: AppBar(
+              backgroundColor: Colors.lightGreen[500],
+              title: Text("Sign Up to ToDo App"),
+              elevation: 0.0,
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
                     },
-                    onChanged: (val) {
-                      setState(() {
-                        email = val;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration:
-                        textInputDecoration.copyWith(hintText: 'Password'),
-                    obscureText: true,
-                    validator: (val) => val.length < 6
-                        ? 'Enter a password 6+ chars long'
-                        : null,
-                    onChanged: (val) {
-                      setState(() {
-                        pass = val;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  RaisedButton(
-                    color: Colors.green[200],
-                    child:
-                        Text('Register', style: TextStyle(color: Colors.white)),
-                    onPressed: () async {
-                      if (_formkey.currentState.validate()) {
-                        dynamic result = await _auth.registerEmail(email, pass);
-
-                        if (result == null) {
+                    icon: Icon(Icons.person),
+                    label: Text("Sign In"))
+              ],
+            ),
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) {
+                          if (val.isEmpty) {
+                            return 'Enter an Email';
+                          } else {
+                            return null;
+                          }
+                        },
+                        onChanged: (val) {
                           setState(() {
-                            error = 'Please enter Valid Email';
+                            email = val;
                           });
-                        }
-                      }
-                    },
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        obscureText: true,
+                        validator: (val) => val.length < 6
+                            ? 'Enter a password 6+ chars long'
+                            : null,
+                        onChanged: (val) {
+                          setState(() {
+                            pass = val;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      RaisedButton(
+                        color: Colors.green[200],
+                        child: Text('Register',
+                            style: TextStyle(color: Colors.white)),
+                        onPressed: () async {
+                          if (_formkey.currentState.validate()) {
+                            setState(() {
+                              loading = true;
+                            });
+                            dynamic result =
+                                await _auth.registerEmail(email, pass);
+
+                            if (result == null) {
+                              setState(() {
+                                loading = false;
+                                error = 'Please enter Valid Email';
+                              });
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      )
+                    ],
                   ),
-                  SizedBox(height: 20.0),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0),
-                  )
-                ],
-              ),
-            )));
+                )));
   }
 }
---
+++ lib/shared/loading.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.lightGreen[200],
+        child: Center(
+            child: SpinKitChasingDots(
+          color: Colors.lightGreen[500],
+          size: 50.0,
+        )));
+  }
+}
